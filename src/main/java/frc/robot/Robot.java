3fb64d4c3a208969317b6508d699d00060d85e1b
dLL44 <homersgreatepics@gmail.com>
2025-01-12 16:15:54 -0500

    whitespace

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -102,7 +102,7 @@ public class Robot extends TimedRobot
     m_robotContainer.setMotorBrake(true);
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
+    // schedule the autonomous command (example)  
     if (m_autonomousCommand != null)
     {
       m_autonomousCommand.schedule();
