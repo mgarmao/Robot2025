487aa143dbd27a15909b01d165c7e3e9ebb2b199
mgarmao <mgarmao830@gmail.com>
2025-01-12 14:03:56 -0500

    Fixing coral imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -167,24 +167,11 @@ public class RobotContainer
       //     drivebase.driveToPose(
       //         new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0)))
       //                         );
-<<<<<<< HEAD
       // driverXbox.y().whileTrue(drivebase.aimAtSpeaker(2));
       // driverXbox.start().whileTrue(Commands.none());
       // driverXbox.back().whileTrue(Commands.none());
       // driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
       // driverXbox.rightBumper().onTrue(Commands.none());
-=======
-      driverXbox.y().whileTrue(drivebase.aimAtSpeaker(2));
-      driverXbox.start().whileTrue(Commands.none());
-      driverXbox.back().whileTrue(Commands.none());
-      // driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
-      // driverXbox.rightBumper().onTrue(Commands.none());
-
-      driverXbox.leftTrigger().onTrue(corl.corlIntake()).onFalse(corl.corlStop());
-      driverXbox.leftBumper().onTrue(corl.corlOuttake()).onFalse(corl.corlStop());
-      driverXbox.rightTrigger().onTrue(algae.algaeIntake()).onFalse(algae.algaeStop());
-      driverXbox.rightBumper().onTrue(algae.algaeOuttake()).onFalse(algae.algaeStop());
->>>>>>> d102a197a794091d3118a3a9e2f1b962630a68c9
     }
 
   }
--- src/main/java/frc/robot/subsystems/Corl.java
+++ src/main/java/frc/robot/subsystems/Corl.java
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-// // coral shitter or something i genuinely don't know
-// // made by yours truly ruben
-=======
+package frc.robot.subsystems;
+
 // coral shitter or something i genuinely don't know
 // made by yours truly ruben
 // mb mike i took a lil long, but i did it so we good.
->>>>>>> d102a197a794091d3118a3a9e2f1b962630a68c9
 
 // package frc.robot.subsystems;
 
@@ -17,23 +14,14 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import frc.robot.Constants;
 
-<<<<<<< HEAD
-// public class Corl extends SubsystemBase {
-
-//     public Corl() {}
-
-//     public Command corlIntake() {
-//         return runOnce(
-//             () -> {
-//                 /* one-time action goes here */
-//             });
-//     }
-// }
-=======
 public class Corl extends SubsystemBase {
     private SparkMaxConfig motorConfig;
     private SparkMax motor;
@@ -69,4 +57,3 @@ public class Corl extends SubsystemBase {
             });
     }
 }
->>>>>>> d102a197a794091d3118a3a9e2f1b962630a68c9
