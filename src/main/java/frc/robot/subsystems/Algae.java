71f1db03c204dd41bc74df84242fd4bfb547be08
dLL44 <homersgreatepics@gmail.com>
2025-01-14 10:42:24 -0500

    removed import

--- src/main/java/frc/robot/subsystems/Algae.java
+++ src/main/java/frc/robot/subsystems/Algae.java
@@ -10,8 +10,6 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
