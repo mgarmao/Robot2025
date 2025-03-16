8dc14a6b3f12b0768fa145eb589ad6db443f8d59
mgarmao <mgarmao830@gmail.com>
2025-03-16 14:55:39 -0400

    Adding cam specs

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -338,30 +338,21 @@ public class Vision
     /**
      * Left Camera
      */
-    LEFT_CAM("cam1",
-             new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(30)),
-             new Translation3d(Units.inchesToMeters(12.056),
-                               Units.inchesToMeters(10.981),
-                               Units.inchesToMeters(8.44)),
+    FRONT_LEFT("cam2",
+             new Rotation3d(0, Math.toRadians(5), Math.toRadians(45)),
+             new Translation3d(Units.inchesToMeters(-10.614),
+                    Units.inchesToMeters(10.152),
+                    Units.inchesToMeters(7.5)),
              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
     /**
      * Right Camera
      */
-    FRONT("cam2",
-              new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
-              new Translation3d(Units.inchesToMeters(12.056),
-                                Units.inchesToMeters(-10.981),
-                                Units.inchesToMeters(8.44)),
-              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
-    /**
-     * Center Camera
-     */
-    BACK("cam4",
-               new Rotation3d(0, Units.degreesToRadians(18), 0),
-               new Translation3d(Units.inchesToMeters(-4.628),
-                                 Units.inchesToMeters(-10.687),
-                                 Units.inchesToMeters(16.129)),
-               VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
+    FRONT_RIGHT("cam4",
+              new Rotation3d(0, Math.toRadians(5), Math.toRadians(-45)),
+              new Translation3d(Units.inchesToMeters(10.614),
+                    Units.inchesToMeters(10.152),
+                    Units.inchesToMeters(7.5)),
+              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
 
     /**
      * Latency alert to use when high latency is detected.
