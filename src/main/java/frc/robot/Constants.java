725a0cba4cad64314f0bd78dccda07c0fbfcb6eb
Rudra <rudypatel2110@gmail.com>
2025-10-17 14:53:04 -0400

    hhhiihh

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -121,9 +121,9 @@ public static final class Setpoints {
   public static final double L1Rotator = 0.5;
   public static final double L1Intake = 0.2;
 
-  public static final double NewIntakeDefault = 2.5;      
-  public static final double NewIntakeIntake = 67.0;    
-  public static final double NewIntakeDispense = 25.0;     
+  public static final double NewIntakeDefault = -2;      
+  public static final double NewIntakeIntake = -67.0;    
+  public static final double NewIntakeDispense = -25.0;     
 }
   public static boolean IsInTele = false;
 
