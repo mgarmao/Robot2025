22b3bc3819d28c267c6f132df3837af5f0affee3
wagubeef <agiannikouris27@stjoes.org>
2025-10-18 07:57:56 -0400

    h

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -263,8 +263,8 @@ public class RobotContainer {
       oppXbox.povLeft().onTrue(INTAKE.Intake(false)).onFalse(INTAKE.HaltIntake());
       oppXbox.povRight().onTrue(INTAKE.Intake(true)).onFalse(INTAKE.HaltIntake());
 
-    oppXbox.leftBumper().onTrue(INTAKE.Rotate_Goto(1)).onFalse(INTAKE.Rotate_Goto(0));
-    oppXbox.rightBumper().onTrue(INTAKE.Rotate_Goto(2)).onFalse(INTAKE.Rotate_Goto(0));
+    oppXbox.leftBumper().onTrue(INTAKE.Rotate_Goto(1)).onFalse(Commands.none());
+    oppXbox.rightBumper().onTrue(INTAKE.Rotate_Goto(2)).onFalse(Commands.none());
 
 
           // oppXbox.a().onTrue(
